d64198eaa9ba7b0642cb78dd91e304560a40332e
Alexandr <95639393+Duhan8alex@users.noreply.github.com>
2021-12-14 05:39:35 +0700

    Add files via upload

---
+++ HomeWorkTTwo.java
@@ -0,0 +1,14 @@
+/**
+ * Java1 HomeWork #1
+ *
+ * @author Dushankovskiy Alexandr
+ * @version 11.12.2021
+ */
+class HomeWorkTTwo {
+    public static void main(String[]arge) {
+        printMain();
+    }
+    static void printMain(int 8) {
+        System.out.println(a >= 0 ? "true" : "false");
+    }
+}
